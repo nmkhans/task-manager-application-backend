aa17f76f3a7a9ed2fcba7e8718b847e9babe86b6
Moin Khan <nurmoin05@gmail.com>
2022-12-08 20:58:40 +0600

    app connection with database added

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require("express");
+require("dotenv").config();
 const router = require("./src/routes/api");
 const cors = require("cors");
+const { default: mongoose } = require("mongoose");
 
 //? app configuration
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+//? database connection
+const uri = process.env.DATABASE_URI;
+const databaseConfig = {
+    user: process.env.DATABASE_USER,
+    pass: process.env.DATABASE_PASS
+};
+
+mongoose.set('strictQuery', true);
+mongoose.connect(uri, databaseConfig)
+    .then(() => console.log("Database connected"))
+    .catch((error) => console.log(error));
 
 //? handle routes
 app.use("/api/v1", router);
